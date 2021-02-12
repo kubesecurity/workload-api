6eaf456fe6c6d400e7e05593e237de3b98afbe61
Avishkar Gupta <avgupta@redhat.com>
2021-02-12 18:54:37 +0530

    Fix tests
    Signed-off-by: Avishkar Gupta <avgupta@redhat.com>

--- tests/routes/test_accounts.py
+++ tests/routes/test_accounts.py
@@ -10,10 +10,11 @@ def test_get_accounts(app_client, monkeypatch, auth_header, mocker, account_obje
     """Test liveness probe."""
     monkeypatch.setattr(src.config, "SECRET_CLIENT_API_TOKEN", "test")
     mock_db_call = mocker.patch("src.routes.accounts.AccountModel")
-    mock_db_call = mock_db_call.query.filter.return_value.order_by
-    mock_db_call.return_value.offset.return_value.limit.return_value.all.return_value = account_objects
     mock_db_call.get_total_accounts.return_value = 25
+    mock_db_call = mock_db_call.query.order_by
+    mock_db_call.return_value.offset.return_value.limit.return_value = account_objects
     response = app_client.get("/api/v1/accounts", headers=auth_header)
     assert response.status_code == 200
     response_json = response.get_json()
     assert "accounts" in response_json
+    assert len(response_json["accounts"]) == 25
